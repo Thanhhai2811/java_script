823405320943e8e52c83d848f7d0786ae0834c3b
Trần Hải <tranthanhhai200000@gmail.com>
2023-05-18 13:04:43 +0700

    done03

---
+++ bai3/js/app.js
@@ -0,0 +1,18 @@
+
+// // "use strict";
+
+// let email = ('aaaa@gmail.com'), // conts | var | let
+// console.log(email);
+
+shoWMessage = () => {
+    "use strict";
+   var message = 'Hello word!'
+    console.log(message)
+}
+
+shoWMessage();
+
+
+
+
+
