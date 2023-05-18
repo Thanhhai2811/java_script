c1f0cfd048ba05b98d9ac2c46407c5cd03ffeb64
Trần Hải <tranthanhhai200000@gmail.com>
2023-05-18 20:30:21 +0700

    done04

---
+++ bai4/js/thuchanh1.js
@@ -0,0 +1,11 @@
+"use strict";
+let name = "Trần Thanh Hải"
+let email ="tranthanhhai20@gmail.com"
+let address = 'Vĩnh Yên-Vĩnh Phúc'
+let phone = '0397794349'
+
+document.write(`Xin Chào ${name} </br>
+        email của bạn là ${email} </br>
+        Địa chỉ của bạn là ${address} </br>
+        Số điện thoại của bạn là ${phone} </br>`)
+
---
+++ bai4/js/thuchanh4.js
@@ -0,0 +1,24 @@
+// let ngaysinh='28'
+// let thangsinh='11'
+// let namsinh='2000'
+
+// document.write(ngaysinh + thangsinh + namsinh)
+
+
+
+
+// let ngaysinh='28'
+// let thangsinh='11'
+// let namsinh='2000'
+
+
+// document.write(ngaysinh + thangsinh + namsinh)
+
+
+
+let ngaysinh='25'
+let thangsinh='54'
+let namsinh='1998'
+
+
+document.write(ngaysinh + thangsinh + namsinh)
