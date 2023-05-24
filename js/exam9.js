c144fef9b0dbb94c95bacd33ef6028d27681f414
Trần Hải <tranthanhhai200000@gmail.com>
2023-05-24 20:36:19 +0700

    done09

---
+++ js/exam9.js
@@ -0,0 +1,30 @@
+// let date = 6;
+// let today = '2';
+
+
+// if (date == 6 && today != 'CN') {
+//   console.log ('Phải đi làm')
+// }   else {
+//     console.log('Nay được nghỉ')
+// }
+
+
+// /* Khách hàng mua với đơn giá 
+// > 500 -> tặng tặng 20%
+// - lớn hơn 200 nhỏ hơn hoặc bằng 500 -> tặng 10%
+// - Nếu mua hàng với đơn hàng nhỏ hơn 200 thì được tặng 5%
+// */
+
+// let order = 400;
+
+// if (order > 500) {          
+//    console.log('bạn được tặng 20%')
+// }  else if (order > 200 && order <= 500) {
+//     console.log('bạn được tặng 10% ')
+// }  else if (order > 100 &&  order <= 200) {
+//     console.log('bạn được tặng 5%')
+// } else {
+//     console.log('bạn không được tặng gì')
+// }  
+
+
